8b054bf8373d8b62bd63767637e46eaf85d60386
von Rudorff, Guido <guido@vonrudorff.de>
2025-09-02 09:55:31 +0200

    Cleanup

--- examples/space/random-molecule.py
+++ examples/space/random-molecule.py
@@ -1,13 +1,11 @@
 import nablachem.space as ncs
 
-c = ncs.ApproximateCounter(show_progress=False)
-s = ncs.SearchSpace()
-s.add_element(ncs.Element("C", [4]))
-s.add_element(ncs.Element("H", [1]))
-s.add_element(ncs.Element("O", [2]))
-s.add_element(ncs.Element("N", [3]))
-s.add_element(ncs.Element("F", [1]))
+counter = ncs.ApproximateCounter(show_progress=False)
+space = ncs.SearchSpace("C:4 H:1 O:1 N:3 F:1")
 
-c.random_sample(s, natoms=10, nmols=3, selection=ncs.Q("C + O + N + F <= 9"))
-c.count(s, 6, ncs.Q("C + O + N + F <= 9"))
-c.count(s, 30, ncs.Q("(C > 10) & (H > 10) & (H + F > 15)"))
+mols = ncs.random_sample(
+    counter, space, natoms=10, nmols=3, selection=ncs.Q("C + O + N + F <= 9")
+)
+
+for mol in mols:
+    print(mol.SMILES)
